3fefafef280a123b19c690963c2e00b2484b8be2
NikoAoi <AmstrongPual@gmail.com>
2019-12-28 16:12:57 +0800

    新增头文件header.h

---
+++ header.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define isSpace(x) isspace((int)(x))
+
+#define PAGE_NOT_FOUND 1
+#define CGI_ERROR 2
+#define REQUEST_ERROR 3
+#define REQUEST_METHOD_ERROR 4
+
+#define SERVER_INFO "Server: tiny_web_server/1.1.0\r\n"
+
+void request_handler(int); 
+         
+void error_handler(int); 
+
+void response_file(int, const char*); 
+
+void initialize_server(u_short *); 
+
+int get_request_line(int, char*, int); 
+
+void execute_cgi(int, const char *,const char *,const char *); 
