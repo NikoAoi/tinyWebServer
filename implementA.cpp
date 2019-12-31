baed26bb06c8a6d98db835d4980b4056acd74ae1
niko <AmstrongPual@gmail.com>
2019-12-31 18:58:11 +0800

    独立main函数

--- implementA.cpp
+++ implementA.cpp
@@ -117,16 +117,4 @@ int get_request_line(int socketfd, char* buf){
     return i;
 }
 
-int main(void){
-	int serverfd, clientfd;
-	pthread_t t1;
-	struct sockaddr_in client_addr;
-	serverfd = initialize_server();
-	while(1){
-		clientfd = accept(serverfd, (struct sockaddr *)&client_addr, &(sizeof(client_addr)));
-		pthread_create(&t1 , NULL, request_handler, clientfd);
-	}
-	close(serverfd);
-    return 0;
-}
 
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "header.h" 
+int main(void){
+	int serverfd, clientfd;
+	pthread_t t1;
+	struct sockaddr_in client_addr;
+	serverfd = initialize_server();
+	while(1){
+		clientfd = accept(serverfd, (struct sockaddr *)&client_addr, &(sizeof(client_addr)));
+		pthread_create(&t1 , NULL, request_handler, clientfd);
+	}
+	close(serverfd);
+    return 0;
+}
